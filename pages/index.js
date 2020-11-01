b504387fd154d092925cb3d286cb97d571d243ce
Leon Cheng <jq153387@Leonde-MacBook-Pro.local>
2020-11-01 21:34:53 +0800

    add prettier and eslint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Head from 'next/head'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function HomePage() {
   return (
@@ -21,42 +21,46 @@ function HomePage() {
         <meta name="copyright" content="Copyright © 2013 CITIAR 版權所有" />
         <title>CJHTML</title>
       </Head>
-        <img src="cjhtml.png" />
+      <img src="cjhtml.png" />
       <Container fluid>
         <Row>
           <Col sm={4}>
             <h3>告訴我你所不要的屬性與標籤</h3>
             <Form>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>屬性</Form.Label>
-                    <Form.Control as="textarea" rows={5} placeholder="class title border cellpadding cellspacing style bordercolor width height valign align bgcolor face size color msimagelist lang" />
-                </Form.Group>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>屬性</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  placeholder="class title border cellpadding cellspacing style bordercolor width height valign align bgcolor face size color msimagelist lang"
+                />
+              </Form.Group>
             </Form>
           </Col>
           <Col sm={4}>
             <h3>在這貼上您所要清潔的HTML</h3>
             <Form>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" rows={5} />
-                </Form.Group>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" rows={5} />
+              </Form.Group>
             </Form>
           </Col>
           <Col sm={4}>
-              <>
-                <Button>開始清潔</Button>
-                <Button variant="light">複制清潔結果</Button> 
-                </>
+            <>
+              <Button>開始清潔</Button>
+              <Button variant="light">複制清潔結果</Button>
+            </>
             <h3>清潔結果</h3>
             <Form>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" rows={5}/>
-                </Form.Group>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" rows={5} />
+              </Form.Group>
             </Form>
           </Col>
         </Row>
       </Container>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
